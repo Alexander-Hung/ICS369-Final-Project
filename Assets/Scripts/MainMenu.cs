d8a40b79a365d01222e0af5ba658eeb93824ff59
khirano7 <khirano7@hawaii.edu>
2023-04-04 23:17:22 -1000

    added fades between scenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 
 public class MainMenu : MonoBehaviour
 {
+
     public void Play()
     {
-        SceneManager.LoadScene("Scene2");
+        StartCoroutine(SceneLoader.instance.LoadLevel("Scene2"));
     }
+
 }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader instance;
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public IEnumerator LoadLevel(string sceneName)
+    {
+        // play the fade animation
+        transition.SetTrigger("Play");
+        yield return new WaitForSeconds(transitionTime);
+
+        // load new scene
+        SceneManager.LoadScene(sceneName);
+    }
+}
