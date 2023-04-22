db6d43bade1aabd2c9d5359bdcdefef6c5f0a986
graquepo <graquepo@hawaii.edu>
2023-04-21 15:02:32 -1000

    added teleporter to Main Map

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,12 +10,18 @@ public class SceneSwitch : MonoBehaviour
     { 
         if (other.gameObject.tag == "Player")
         {
-            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Scene1"))
+            
+            /**if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Scene1"))
             {
                 SceneManager.LoadScene(1);
             }
 
             if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Scene2"))
+            {
+                SceneManager.LoadScene(0);
+            }
+            **/
+            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("BossScene"))
             {
                 SceneManager.LoadScene(0);
             }
