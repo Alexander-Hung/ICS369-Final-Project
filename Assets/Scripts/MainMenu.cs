138d6b2308c67cd274aaa3a1155e6d0dc0165917
Alexander Hung <hungalex@hawaii.edu>
2023-04-30 10:44:57 -1000

    restore

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,23 +5,8 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public List<GameObject> Image;
-
-    private float elapsedTime;
-
     public void Play()
     {
-        foreach (var item in Image)
-        {
-            elapsedTime += Time.deltaTime;
-            if (elapsedTime >= 1f)
-            {
-                elapsedTime = 0f;
-                item.gameObject.SetActive(true);
-            }
-        }
-
-
         StartCoroutine(SceneLoader.instance.LoadLevel("MainScene"));
     }
 
